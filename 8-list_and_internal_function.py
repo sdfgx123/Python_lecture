8848a71e4daa7b4e0818ff89e7a5d980f6032374
sdfgx123 <sdfgx123@naver.com>
2021-05-15 00:45:45 +0900

    리스트와 내장함수 두번째 강의

--- 8-list_and_internal_function.py
+++ 8-list_and_internal_function.py
@@ -49,3 +49,43 @@ a.sort(reverse=True) # 내림차순 정렬
 print(a)
 a.clear()
 print(a) # 데이터 다 날림
+
+# 8-2. 리스트와 내장함수 두번째 강의, 20210515
+a = [23, 12, 36, 53, 19]
+print(a[:3])
+print(len(a)) # a 리스트의 길이 출력
+
+for i in range(len(a)):
+    print(a[i], end=' ') # a 리스트의 길이만큼 차례대로 출력하는데, 구분은 띄어서 해라
+print()
+
+for x in a: # 위 for문이랑 똑같은 결과
+    print(x, end=' ')
+print()
+
+for x in a: # 홀수만 출력
+   if x%2==1:
+       print(x, end=' ')
+print()
+
+# a 리스트를 튜플 형태로 저장해서 출력
+for x in enumerate(a): # enumerate : 튜플 형태로 데이터를 바꾸는 함수인 듯?
+    print(x)
+
+for x in enumerate(a):
+    print(x[0], x[1])
+print()
+
+for index, value in enumerate(a):
+    print(index, value)
+print()
+
+if all(50 > x for x in a): # all : 조건이 모두 참일 때
+    print('YES')
+else:
+    print('NO')
+
+if any(15 > x for x in a): # any : 하나라도 참인 게 있을 때
+    print('YES')
+else:
+    print('NO')
