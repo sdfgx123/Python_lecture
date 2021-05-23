8c7a079ea04433294a52f86cd3dc9fea7c4fb1d3
sdfgx123 <sdfgx123@naver.com>
2021-05-24 00:31:51 +0900

    10

--- section_3/9.py
+++ section_3/9.py
@@ -13,6 +13,6 @@ for x in a:
 cnt=0
 for i in range(1, n+1):
     for j in range(1, n+1):
-        if all(a[i][j]>a[i+dx[k]][j+dy[k]] for k in range(4)):
+        if all(a[i][j]>a[i+dx[k]][j+dy[k]] for k in range(4)): # all : 조건 모두 참일 때
             cnt+=1
 print(cnt)
