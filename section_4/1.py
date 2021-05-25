3bfeafa247c3b66a99e19ef974398f85612d60ec
sdfgx123 <sdfgx123@naver.com>
2021-05-26 01:05:24 +0900

    section 4 : 1

---
+++ section_4/1.py
@@ -0,0 +1,17 @@
+# 이분검색
+import sys
+sys.stdin=open("C:\python_lecture\Python_lecture\section_4\input.txt", "rt")
+n, m = map(int, input().split())
+a = list(map(int, input().split()))
+a.sort()
+lt=0
+rt=n-1
+while lt<=rt:
+    mid=(lt+rt)//2
+    if a[mid]==m:
+        print(mid+1)
+        break
+    elif a[mid]>m:
+        rt=mid-1
+    else:
+        lt=mid+1
