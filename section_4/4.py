68c9d08770fa343f197a8b570c18fd713f686eda
sdfgx123 <sdfgx123@naver.com>
2021-05-30 21:45:44 +0900

    4-4

---
+++ section_4/4.py
@@ -0,0 +1,27 @@
+# 마구간 정하기(결정알고리즘)
+import sys
+sys.stdin=open("C:\python_lecture\Python_lecture\section_4\input.txt", "rt")
+def Count(len):
+    cnt=1
+    ep=Line[0] # ep=endpoint
+    for i in range(1, n):
+        if Line[i]-ep>=len:
+            cnt+=1
+            ep=Line[i]
+    return cnt
+n, c=map(int, input().split())
+Line=[]
+for _ in range(n):
+    tmp=int(input())
+    Line.append(tmp)
+Line.sort()
+lt=1
+rt=Line[n-1]
+while lt<=rt:
+    mid=(lt+rt)//2
+    if Count(mid)>=c:
+        res=mid
+        lt=mid+1
+    else:
+        rt=mid-1
+print(res)
