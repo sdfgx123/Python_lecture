ab164ad7894220462ecbc75f8867495a150f71db
sdfgx123@naver.com <sdfgx123@naver.com>
2021-06-08 10:30:03 +0900

    5-2, 3, 4

---
+++ section_5/4.py
@@ -0,0 +1,26 @@
+# 후위표기식 연산
+import sys
+sys.stdin=open("C:\Python-lecture\Python_lecture\section_5\input.txt", "rt")
+a=input() # input 바로 받으면 Type은 String
+stack=[]
+for x in a:
+    if x.isdecimal():
+        stack.append(int(x))
+    else:
+        if x=='+':
+            n1=stack.pop()
+            n2=stack.pop()
+            stack.append(n2+n1)
+        elif x=='-':
+            n1=stack.pop()
+            n2=stack.pop()
+            stack.append(n2-n1)
+        elif x=='*':
+            n1=stack.pop()
+            n2=stack.pop()
+            stack.append(n2*n1)
+        elif x=='/':
+            n1=stack.pop()
+            n2=stack.pop()
+            stack.append(n2/n1)
+print(stack[0])
---
+++ section_5/5.py
@@ -0,0 +1,15 @@
+# 공주 구하기(큐), FIFO, 선입선출
+import sys
+from collections import deque
+sys.stdin=open("C:\Python-lecture\Python_lecture\section_5\input.txt", "rt")
+n, k=map(int, input().split())
+dq=list(range(1, n+1))
+dq=deque(dq)
+while dq:
+    for _ in range(k-1):
+        cur=dq.popleft()
+        dq.append(cur)
+    dq.popleft()
+    if len(dq)==1:
+        print(dq[0])
+        dq.popleft()
