09b8009cdb9bd287e9c9280dbc7014dc7259a5a4
sdfgx123@naver.com <sdfgx123@naver.com>
2021-06-29 16:47:42 +0900

    6-2 ongoing

---
+++ section_6/2.py
@@ -0,0 +1,7 @@
+# 이진트리 순회 (깊이우선탐색)
+# 전위순회 : 부왼오, 중위순회 : 왼부오, 후위순회 : 왼오부
+import sys
+sys.stdin=open("C:\Python-lecture\Python_lecture\section_6\input.txt", "rt")
+
+if __name__=="__main__":
+    DFS(1)
