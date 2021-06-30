4f05068e3b06a895f5419d6af53500abbd679d89
sdfgx123@naver.com <sdfgx123@naver.com>
2021-06-30 16:00:50 +0900

    6-1

--- section_6/2.py
+++ section_6/2.py
@@ -2,6 +2,24 @@
 # 전위순회 : 부왼오, 중위순회 : 왼부오, 후위순회 : 왼오부
 import sys
 sys.stdin=open("C:\Python-lecture\Python_lecture\section_6\input.txt", "rt")
+def DFS(v):
+    if v>7:
+        return
+    else:
+        # 전위순회
+        # print(v, end=" ")
+        # DFS(v*2)
+        # DFS(v*2+1)
+
+        # 중위순회
+        # DFS(v*2)
+        # print(v, end=" ")
+        # DFS(v*2+1)
+
+        # 후위순회, 예시 : 병합정렬
+        DFS(v*2)
+        DFS(v*2+1)
+        print(v, end=" ")
 
 if __name__=="__main__":
     DFS(1)
