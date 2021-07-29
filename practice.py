c2269a5124cc92140db951a6166d40d4440a5071
sdfgx123 <sdfgx123@naver.com>
2021-07-29 16:52:27 +0900

    practice editted

--- practice.py
+++ practice.py
@@ -1,4 +1,7 @@
-a, d, n=map(int, input().split())
-if n==1:
-    print(a)
-print(a+(n-1)*d)
+import sys
+n=int(input())
+a=[]
+for i in range(n):
+    a.append(int(sys.stdin.readline()))
+for i in sorted(a):
+    sys.stdout.write(str(i)+'\n')
