2a553c06f285c6d1363846eedf33061e65c6b713
sdfgx123 <sdfgx123@naver.com>
2021-07-31 15:48:53 +0900

    practice editted

--- practice.py
+++ practice.py
@@ -1,12 +1,5 @@
-def solution(n):
-    global sum
-    for i in range(1, n+1):
-        if n%i==0:
-            sum+=i
-    answer=sum
-    return answer
-
 n=int(input())
-sum=0
-sum=solution(n)
-print(sum)
+arr=[]
+for _ in range(n):
+    arr.append(int(input()))
+print(arr)
