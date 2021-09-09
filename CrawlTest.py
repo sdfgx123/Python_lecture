1fa54f7b56f727d14fa376712a10e3bdf633deba
sdfgx123 <sdfgx123@naver.com>
2021-09-09 15:44:08 +0900

    210909

--- CrawlTest.py
+++ CrawlTest.py
@@ -1,15 +1,22 @@
 # Inflearn 파이썬 크롤링 테스트
 import requests
 from bs4 import BeautifulSoup
+import sys
+f=open("test_5th.scd", 'w')
 
 headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.63 Safari/537.36'}
-url = "https://finance.naver.com/item/board.nhn?code=263800"
-
+url = "http://www.kyobobook.co.kr/bestSellerNew/bestseller.laf?orderClick=d79"
 r = requests.get(url, headers=headers)
 r.status_code
-
 soup = BeautifulSoup(r.text, 'html.parser')
-
-title = soup.find_all('td', class_='title')
-for i in title:
-    print(i.text)
+book_info = soup.find_all('div', class_='detail')
+title=[]
+subtitle=[]
+author=[]
+for info in book_info:
+    tmp=info.find('a')
+    title=tmp.find('strong')
+    res="^title: "+title.get_text()
+    f.write(res)
+    print('\n')
+f.close()
