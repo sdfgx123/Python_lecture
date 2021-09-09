bf61a961c7e234b7a6a8302c46629a39f47e4b9f
sdfgx123 <sdfgx123@naver.com>
2021-09-09 16:34:12 +0900

    title, subtitle testing done

--- CrawlTest.py
+++ CrawlTest.py
@@ -1,9 +1,8 @@
-# Inflearn 파이썬 크롤링 테스트
 import requests
 from bs4 import BeautifulSoup
 import sys
-f=open("test_5th.scd", 'w')
-
+# scd 파일 생성 / 위치 : 파이썬 설치 경로
+f=open("test_11th.scd", 'w')
 headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.63 Safari/537.36'}
 url = "http://www.kyobobook.co.kr/bestSellerNew/bestseller.laf?orderClick=d79"
 r = requests.get(url, headers=headers)
@@ -14,9 +13,11 @@ title=[]
 subtitle=[]
 author=[]
 for info in book_info:
-    tmp=info.find('a')
-    title=tmp.find('strong')
-    res="^title: "+title.get_text()
+    tmp_title=info.find('a')
+    tmp_subtitle=info.find('div', class_="subtitle")
+    title=tmp_title.find('strong')
+    res="^title: "+title.get_text()+"\n"
     f.write(res)
+    f.write("^content: "+tmp_subtitle.get_text().strip()+"\n")
     print('\n')
 f.close()
